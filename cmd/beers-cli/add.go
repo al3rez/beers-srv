0e5277d90c07d04108e7aecab6c3d0ce7c3df30b
Alireza Bashiri <azbshiri@gmail.com>
2019-10-21 03:33:58 +0700

    fix(add)!: strip name before adding

--- cmd/beers-cli/add.go
+++ cmd/beers-cli/add.go
@@ -3,14 +3,29 @@ package main
 import (
 	"context"
 	"fmt"
+	"regexp"
 
 	"github.com/azbshiri/beers-proto/pkg/pb"
 	"google.golang.org/grpc"
 )
 
+func strip(str string) (string, error) {
+	reg, err := regexp.Compile("[^a-zA-Z0-9]+")
+	if err != nil {
+		return "", err
+	}
+
+	return reg.ReplaceAllString(str, ""), nil
+}
+
 func add(ctx context.Context, cc *grpc.ClientConn, client pb.BeersClient, name *string) (*pb.BeerAddResponse, error) {
+	n, err := strip(*name)
+	if err != nil {
+		return nil, fmt.Errorf("strip: %s\n", err)
+	}
+
 	req := &pb.BeerAddRequest{
-		Name: *name,
+		Name: n,
 	}
 
 	resp, err := client.Add(ctx, req)
